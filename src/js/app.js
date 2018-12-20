ee199ff34e6640235c675b2e8c0fc470591a8b17
wrogoz <wrogoz@gmail.com>
2018-12-20 21:49:05 +0100

    dodano ikony do prognozy dzis/jutro

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,17 @@
 
 const input = $('#inputLGEx');
 const thisDay=$('#this-day');
-const cityName=$('#city-name')
+const thisDayImg = $('#this-day__img');
+const tommorow = $('#tommorow-day');
+const tommorowImg=$('#tommorow-day__img')
+const cityName=$('#city-name');
+
 input.keypress(function (e) {
     if (e.which == 13) {
+
+        //today
+
+
         let weatherbit = `https://api.weatherbit.io/v2.0/current?city=${input.val()}&key=7e5cfb6fc82247268e54b66455f03017`;
         $.ajax({
             url: weatherbit
@@ -11,24 +19,40 @@ input.keypress(function (e) {
             method: 'GET'
 
         }).done((resp) => {
-            thisDay.text(` temp: ${resp.data[0].temp} C`);
+            thisDay.text(` Dziś: ${resp.data[0].temp} C`);
+            thisDayImg.attr('src', `src/images/${resp.data[0].weather.icon}.png`)
             cityName.addClass('fadeOutUp');
             setTimeout(function () {
                 cityName.text(`${resp.data[0].city_name}`);
-                cityName.addClass('fadeInUpBig');
+                cityName.addClass('fadeInUp');
                 setTimeout(function () {
                     input.val("");
                 }, 500);
                 
             }, 1000);
             setTimeout(function() {
-                cityName.removeClass('fadeInUpBig');
+                cityName.removeClass('fadeInUp');
                 cityName.removeClass('fadeOutUp');
             }, 2000);
             
             }).fail((err) => {
                 console.log('error')
             });
+
+            //tommorow wheather
+
+        let weatherNextDay = `https://api.weatherbit.io/v2.0/forecast/daily?city=${input.val()}&key=&key=7e5cfb6fc82247268e54b66455f03017&days=2`;
+        $.ajax({
+            url: weatherbit
+            ,
+            method: 'GET'
+
+        }).done((resp) => {
+            tommorow.text(` Jutro: ${resp.data[0].temp} C`);
+            tommorowImg.attr('src', `src/images/${resp.data[0].weather.icon}.png`)
+        }).fail((err)=>{
+            console.log('next day forecast error');
+        })
 
     }
     
