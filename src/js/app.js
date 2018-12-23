a83f4383094f48e7b5db83d26ae51da6b7efb867
wrogoz <wrogoz@gmail.com>
2018-12-23 22:18:07 +0100

    dodano nowe ikony , poprawiono literówki

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ const thisDay=$('#this-day');
 const thisDayImg = $('#this-day__img');
 const tommorow = $('#tommorow-day');
 const tommorowImg=$('#tommorow-day__img')
+const afterTommorowDay = $('#after-tommorow-day');
+const afterTommorowDayImg = $('#after-tommorow-day__img');
 const cityName=$('#city-name');
 
 input.keypress(function (e) {
@@ -20,7 +22,7 @@ input.keypress(function (e) {
 
         }).done((resp) => {
             thisDay.text(` Dziś: ${resp.data[0].temp} C`);
-            thisDayImg.attr('src', `src/images/${resp.data[0].weather.icon}.png`)
+            thisDayImg.attr('src', `src/images/${resp.data[0].weather.icon}.svg`)
             cityName.addClass('fadeOutUp');
             setTimeout(function () {
                 cityName.text(`${resp.data[0].city_name}`);
@@ -39,7 +41,7 @@ input.keypress(function (e) {
                 console.log('error')
             });
 
-            //tommorow wheather
+            //tommorow weather
 
         let weatherNextDay = `https://api.weatherbit.io/v2.0/forecast/daily?city=${input.val()}&key=7e5cfb6fc82247268e54b66455f03017&days=1`;
         $.ajax({
@@ -49,8 +51,22 @@ input.keypress(function (e) {
 
         }).done((resp) => {
             tommorow.text(` Jutro: ${resp.data[0].temp} C`);
-            tommorowImg.attr('src', `src/images/${resp.data[0].weather.icon}.png`)
+            tommorowImg.attr('src', `src/images/${resp.data[0].weather.icon}.svg`)
         }).fail((err)=>{
+            console.log('next day forecast error');
+        })
+        //after tommorow weather
+
+        let weatherafterTommorow = `https://api.weatherbit.io/v2.0/forecast/daily?city=${input.val()}&key=7e5cfb6fc82247268e54b66455f03017&days=2`;
+        $.ajax({
+            url: weatherafterTommorow
+            ,
+            method: 'GET'
+
+        }).done((resp) => {
+            afterTommorowDay.text(` Pojutrze: ${resp.data[0].temp} C`);
+            afterTommorowDayImg.attr('src', `src/images/${resp.data[0].weather.icon}.svg`)
+        }).fail((err) => {
             console.log('next day forecast error');
         })
 
